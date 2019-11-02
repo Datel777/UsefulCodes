0de2684202a82240d8b222600e46e4150d1c2ead
Datel777 <Datel777@users.noreply.github.com>
2019-11-02 16:06:14 +0300

    Added debug.h, Fastest is Even/isOdd

---
+++ Fastest isEven_isOdd/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+#include "../debug.h"
+
+//fastest way to check odd or even
+//also, added template to work with every integral types (int, short, long, char)
+template<class T>
+constexpr inline bool isEven(T a)
+{
+    static_assert(is_integral<T>::value, "Argument must be of integral type");
+    return !(a & 1); //or !(a % 2) the are the same
+}
+
+template<class T>
+constexpr inline bool isOdd(T a)
+{
+    static_assert(is_integral<T>::value, "Argument must be of integral type");
+    return a & 1; //or a % 2 the are the same
+}
+
+
+
+int main()
+{
+
+    DEBUG(isEven(0));
+    DEBUG(isEven(1));
+    DEBUG(isEven(2));
+    DEBUG(isEven(5));
+
+    DEBUG(isOdd(0));
+    DEBUG(isOdd(1));
+    DEBUG(isOdd(2));
+    DEBUG(isOdd(5));
+
+    //custom runtime test
+    cout << "=========" << endl;
+    
+    int value;
+    cin >> value;
+    
+    DEBUG(isEven(value));
+    
+    return 0;
+}
---
+++ debug.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//define this to remove debug messages
+//#define NO_DEBUG
+
+
+#ifdef NO_DEBUG
+    #define DEBUG(a)
+#else
+    #define DEBUG(a) std::cout << #a << " = " << a << std::endl
+#endif
